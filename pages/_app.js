961bf7cbda76166de30bbd1e570c12d4f53a32f7
José Henrique Roveda <46685746+josehenriqueroveda@users.noreply.github.com>
2024-12-27 18:02:27 +0000

    feat: base layout for `_app/js`

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,31 @@
 import "../src/styles/globals.css";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  if (router.pathname === "/login") {
+    return <Component {...pageProps} />;
+  }
+
+  return (
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        <Component {...pageProps} />
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <ul className="menu bg-primary text-base-content min-h-full w-80 p-4">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/status">Status</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
