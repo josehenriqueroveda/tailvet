bed8b2a63d4179c4eb5a23b4cc3155b5de5129cc
José Henrique Roveda <46685746+josehenriqueroveda@users.noreply.github.com>
2025-01-10 18:42:18 +0000

    feat: make drawer menu expand or not depending on screen size

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,17 @@ import Link from "next/link";
 import {
   LuArrowUpDown,
   LuHouse,
+  LuMenu,
   LuPawPrint,
-  LuPersonStanding,
+  LuSheet,
+  LuUser,
 } from "react-icons/lu";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   const [loading, setLoading] = useState(true);
   const [authenticated, setAuthenticated] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(true);
 
   useEffect(() => {
     const checkAuth = async () => {
@@ -30,10 +33,24 @@ export default function App({ Component, pageProps }) {
     checkAuth();
   }, [router]);
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setIsDrawerOpen(false);
+      } else {
+        setIsDrawerOpen(true);
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   if (loading) {
     return (
-      <div className="flex justify-center items-center h-screen">
-        Loading...
+      <div className="flex flex-row min-h-screen justify-center items-center">
+        <span className="loading loading-dots loading-lg"></span>
       </div>
     );
   }
@@ -45,35 +62,49 @@ export default function App({ Component, pageProps }) {
   return router.pathname === "/login" ? (
     <Component {...pageProps} />
   ) : (
-    <div className="drawer lg:drawer-open">
-      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content">
-        <Component {...pageProps} />
-      </div>
-      <div className="drawer-side">
-        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-        <ul className="menu bg-primary text-base-content min-h-full w-80 p-4">
+    <div className="flex h-screen">
+      <div
+        className={`${
+          isDrawerOpen ? "w-80" : "w-16"
+        } bg-primary text-white flex flex-col transition-all duration-300`}
+      >
+        <button
+          className="p-4 focus:outline-none"
+          onClick={() => setIsDrawerOpen(!isDrawerOpen)}
+        >
+          <LuMenu size={24} />
+        </button>
+        <ul className="menu flex-grow">
           <li>
             <Link className="text-base text-white" href="/">
-              <LuHouse /> Página Inicial
+              <LuHouse /> {!isDrawerOpen ? null : "Página Inicial"}
             </Link>
           </li>
           <li>
             <Link className="text-base text-white" href="/customers">
-              <LuPersonStanding /> Clientes
+              <LuUser /> {!isDrawerOpen ? null : "Clientes"}
             </Link>
           </li>
           <li>
             <Link className="text-base text-white" href="/pets">
-              <LuPawPrint /> Pets
+              <LuPawPrint /> {!isDrawerOpen ? null : "Pets"}
+            </Link>
+          </li>
+          <li>
+            <Link className="text-base text-white" href="/services">
+              <LuSheet /> {!isDrawerOpen ? null : "Serviços"}
             </Link>
           </li>
           <li>
             <Link className="text-base text-white" href="/status">
-              <LuArrowUpDown /> Status
+              <LuArrowUpDown /> {!isDrawerOpen ? null : "Status"}
             </Link>
           </li>
         </ul>
+      </div>
+
+      <div className="flex-grow">
+        <Component {...pageProps} />
       </div>
     </div>
   );
