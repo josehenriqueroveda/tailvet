e8bda31fa01a8e99ad70046bde74685804eac3ab
José Henrique Roveda <46685746+josehenriqueroveda@users.noreply.github.com>
2025-01-10 18:41:28 +0000

    feat: add migration for `services` table

---
+++ src/infra/migrations/1736532609516_services.js
@@ -0,0 +1,42 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable("services", {
+    id: "id",
+    name: { type: "varchar(255)", notNull: true },
+    price: { type: "decimal(6, 2)" },
+    amount: { type: "integer" },
+    unit: { type: "varchar(20)" },
+    category: { type: "varchar(60)" },
+    is_active: {
+      type: "boolean",
+      notNull: true,
+      default: true,
+    },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+    updated_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {};
