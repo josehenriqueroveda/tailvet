f4c6c5d18da9da951273fab14a2236abe564286c
José Henrique Roveda <46685746+josehenriqueroveda@users.noreply.github.com>
2025-02-28 19:05:31 +0000

    feat: add `/api/v1/blood_tests` endpoint

--- src/pages/api/v1/blood_tests/index.js
+++ src/pages/api/v1/blood_tests/index.js
@@ -142,10 +142,8 @@ async function handlePost(request, response) {
         platelet_count,
         hematozoa_research,
         plasma_protein,
-        comments,
-        created_at,
-        updated_at
-    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28) RETURNING *`,
+        comments
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26) RETURNING *`,
     values: [
       owner_id,
       pet_id,
